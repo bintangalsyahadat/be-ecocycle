8c3d61c31dfc976da7acda7b70b7dfb9fe1b3b5e
BintangAl <bintangalsyahadat1@gmail.com>
2026-07-07 12:22:23 +0700

    fix: add wire:submit save to branch profile form view

--- resources/views/livewire/branch-profile-form.blade.php
+++ resources/views/livewire/branch-profile-form.blade.php
@@ -1,3 +1,9 @@
-<div>
+<form wire:submit="save" class="fi-sc-form">
     {{ $this->form }}
-</div>
+
+    <div class="fi-ac fi-align-end">
+        <x-filament::button type="submit">
+            {{ __('filament-edit-profile::default.save') }}
+        </x-filament::button>
+    </div>
+</form>
